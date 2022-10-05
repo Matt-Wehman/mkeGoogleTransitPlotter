e7b25ca2961ea639af2d00de82d7f318097a4a79
bassoc <bassoc@msoe.edu>
2022-10-05 12:28:14 -0500

    first commit

---
+++ Route.java
@@ -0,0 +1,44 @@
+
+
+/**
+ * @author czerkisi
+ * @version 1.0
+ * @created 05-Oct-2022 12:07:09 PM
+ */
+public class Route {
+
+	private String agencyID;
+	private int longName;
+	private int routeColor;
+	private String routeDesc;
+	private int routeID;
+	private String routeLongName;
+	private int routeShortName;
+	private int routeTextColor;
+	private int routeType;
+	private int routeURL;
+	private HashMap<Integer, Stop> stops;
+	private HashMap<Integer, Trip> trips;
+	public Controller m_Controller;
+
+	public Route(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	private int displayDist(){
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param newRoute
+	 */
+	public boolean update(Route newRoute){
+		return false;
+	}
+
+}
