d24bf312debcbc43ac7a50e28916b149874b6dde
mcdonaldp <mcdonaldp@msoe.edu>
2022-10-17 12:41:28 -0500

    Changed lat/lon specifications and changed test for validateLinesInStop relating to those specifications

--- src/Controller.java
+++ src/Controller.java
@@ -502,7 +502,8 @@ public class Controller {
             String description = reader.next();
             double lat = reader.nextDouble();
             double lon = reader.nextDouble();
-            if(lat == -1 || lon == -1){
+            if(lat == -1 || lon == -1 || (lat < -90.00 || lat > 90.00) ||
+                    (lon < -180.00 || lon > 180.00)){
                 throw new NumberFormatException("empty");
             }
 
